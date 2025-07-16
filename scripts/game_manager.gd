aec208bfa596476c1206115831a067ee1fec034d
Harsh Narayan Jha <harshnj@proton.me>
2025-07-16 18:16:07 +0530

    connect scenes + setup export

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,13 +6,28 @@ extends Node
 @export var antigravities: Array[Antigravity]
 @export var build_mode: bool = true
 
+@export_category("Win/Lose")
+@export_range(0, 10) var win_wait_secs: float = 3.0
+@export_range(0, 50) var lose_wait_secs: float = 10.0
+@export var win_button: TriggerButton
+@export var next_level: PackedScene
+
 var lvel: Vector3
 var avel: Vector3
+var lose_timer: SceneTreeTimer
 
 func _ready() -> void:
+	SceneManager.fade_in()
 	enter_build_mode()
+	win_button.turned_on.connect(win)
+
+func _exit_tree() -> void:
+	win_button.turned_on.disconnect(win)
 
 func enter_build_mode():
+	if lose_timer:
+		lose_timer.timeout.disconnect(lose)
+
 	build_mode = true
 	disable_physics()
 	for s in sections:
@@ -35,6 +50,28 @@ func enter_play_mode():
 		a.lock_change()
 
 	MusicPlayer.play_level_music()
+	# start the lose timer
+	lose_timer = get_tree().create_timer(lose_wait_secs)
+	lose_timer.timeout.connect(lose)
+
+func win() -> void:
+	if lose_timer:
+		lose_timer.timeout.disconnect(lose)
+
+	await get_tree().create_timer(win_wait_secs).timeout
+
+	# Can't use change_scene directly since phantom camera bugs out
+	SceneManager.fade_out()
+	await SceneManager.fade_complete
+
+	get_tree().change_scene_to_packed(next_level)
+
+func lose() -> void:
+	# Can't use change_scene directly since phantom camera bugs out
+	SceneManager.fade_out()
+	await SceneManager.fade_complete
+
+	get_tree().reload_current_scene()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
@@ -45,11 +82,11 @@ func _input(event: InputEvent) -> void:
 				enter_build_mode()
 
 func disable_physics():
+	marble.set_freeze_enabled(true)
 	lvel = marble.linear_velocity
 	avel = marble.angular_velocity
-	marble.set_freeze_enabled(true)
 
 func enable_physics():
-	marble.set_freeze_enabled(false)
 	marble.linear_velocity = lvel
 	marble.angular_velocity = avel
+	marble.set_freeze_enabled(false)
--- scripts/main_menu_manager.gd
+++ scripts/main_menu_manager.gd
@@ -1,5 +1,24 @@
 extends Node
 
+@export var play_button: Button
+@export var exit_button: Button
+@export var level1_scene: PackedScene
 
 func _ready() -> void:
 	MusicPlayer.play_menu_music()
+	play_button.pressed.connect(go_to_level1)
+	exit_button.pressed.connect(exit)
+
+func _exit_tree() -> void:
+	play_button.pressed.disconnect(go_to_level1)
+	exit_button.pressed.disconnect(exit)
+
+func go_to_level1() -> void:
+	# Can't use change_scene directly since phantom camera bugs out
+	SceneManager.fade_out()
+	await SceneManager.fade_complete
+	get_tree().change_scene_to_packed(level1_scene)
+
+func exit() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit(0)
